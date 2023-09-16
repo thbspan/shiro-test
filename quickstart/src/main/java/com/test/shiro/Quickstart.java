933aac36d53fc93ce8db71df4cc0780a30ef872e
thbspan <thbspan@163.com>
2023-09-16 21:20:31 +0800

    update Quickstart demo

--- quickstart/src/main/java/com/test/shiro/Quickstart.java
+++ quickstart/src/main/java/com/test/shiro/Quickstart.java
@@ -1,3 +1,5 @@
+package com.test.shiro;
+
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.config.IniSecurityManagerFactory;
@@ -16,7 +18,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Quickstart {
 
-    private static final transient Logger log = LoggerFactory.getLogger(Quickstart.class);
+    private static final Logger log = LoggerFactory.getLogger(Quickstart.class);
 
 
     public static void main(String[] args) {
